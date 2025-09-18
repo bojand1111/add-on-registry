fb7232e54711bf5b469cd95f768e6faf89ad5563
stirante <brzozowski.s.piotr@gmail.com>
2025-09-18 06:31:12 +0200

    feat: add more add-ons

--- registry.js
+++ registry.js
@@ -26,4 +26,32 @@ export const Registry = {
         name: "Cave Dweller",
         creator: "XP GAMES"
     },
+    ulkd_ess: {
+        name: "Essentials",
+        creator: "UNLINKED"
+    },
+    ulkd_alch: {
+        name: "Alchemy",
+        creator: "Unlinked"
+    },
+    aria_pp: {
+        name: "Planes Pro",
+        creator: "AriaCreations"
+    },
+    darkosto_elemental_crops: {
+        name: "Elemental Crops",
+        creator: "Darkosto"
+    },
+    nps_mot: {
+        name: "More Ores and Tools",
+        creator: "Netherpixel"
+    },
+    pod_farm: {
+        name: "FARMING",
+        creator: "Podcrash"
+    },
+    bf_rb: {
+        name: "Biomes",
+        creator: "Block Factory"
+    },
 };
