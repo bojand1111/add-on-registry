348d5fe02f9a3729684aa9ff25800a776d614a22
8Crafter <134829412+Andexter8@users.noreply.github.com>
2025-09-17 22:16:57 -0700

    Update registry.js
    Added a few more add-ons and added the missing trailing commas.

--- registry.js
+++ registry.js
@@ -4,54 +4,66 @@
 export const Registry = {
     spark_portals: {
         name: "Spark Portals",
-        creator: "Spark Universe"
+        creator: "Spark Universe",
     },
     hf_mzs: {
         name: "Mowzie’s Mobs",
-        creator: "Honeyfrost"
+        creator: "Honeyfrost",
     },
     ascent_paint: {
         name: "Paint",
-        creator: "ASCENT"
+        creator: "ASCENT",
     },
     xp_furniture: {
         name: "Furniture",
-        creator: "XP GAMES"
+        creator: "XP GAMES",
     },
     xp_dl: {
         name: "Dynamic Light",
-        creator: "XP GAMES"
+        creator: "XP GAMES",
     },
     xp_cd: {
         name: "Cave Dweller",
-        creator: "XP GAMES"
+        creator: "XP GAMES",
     },
     ulkd_ess: {
         name: "Essentials",
-        creator: "UNLINKED"
+        creator: "UNLINKED",
     },
     ulkd_alch: {
         name: "Alchemy",
-        creator: "Unlinked"
+        creator: "Unlinked",
     },
     aria_pp: {
         name: "Planes Pro",
-        creator: "AriaCreations"
+        creator: "AriaCreations",
     },
     darkosto_elemental_crops: {
         name: "Elemental Crops",
-        creator: "Darkosto"
+        creator: "Darkosto",
     },
     nps_mot: {
         name: "More Ores and Tools",
-        creator: "Netherpixel"
+        creator: "Netherpixel",
     },
     pod_farm: {
         name: "FARMING",
-        creator: "Podcrash"
+        creator: "Podcrash",
     },
     bf_rb: {
         name: "Biomes",
-        creator: "Block Factory"
+        creator: "Block Factory",
+    },
+    andexsa: {
+        name: "8Crafter's Entity Scale, NBT, and Behavior Modifier, Bossbar, and Morph Addon",
+        creator: "8Crafter",
+    },
+    andexdb: {
+        name: "8Crafter's Server Utilities & Debug Sticks",
+        creator: "8Crafter",
+    },
+    wypnt_ef: {
+        name: "Epic Fantasy",
+        creator: "Waypoint Studios",
     },
 };
