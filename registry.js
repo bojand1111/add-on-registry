19db7ff528e9af57bd2f9e1cedd175c06adb90f8
Thijs Hankel <72493994+ThijsHankelMC@users.noreply.github.com>
2025-09-18 23:18:19 +0200

    feat: switch system to use data file per creator and generate registry

--- registry.js
+++ registry.js
@@ -1,4 +1,7 @@
 /**
+ * AUTO-GENERATED FILE! DO NOT EDIT!
+ * Run `npm run build` to regenerate this file from /registry/*.json
+ *
  * @type {import("./types").RegistryMap}
  */
 export const Registry = {
@@ -30,41 +33,29 @@ export const Registry = {
         name: "Spark Portals",
         creator: "Spark Universe",
     },
-    hf_mzs: {
-        name: "Mowzie’s Mobs",
-        creator: "Honeyfrost",
-    },
-    ascent_paint: {
-        name: "Paint",
-        creator: "ASCENT",
-    },
-    xp_furniture: {
-        name: "Furniture",
-        creator: "XP GAMES",
-    },
-    xp_dl: {
-        name: "Dynamic Light",
-        creator: "XP GAMES",
-    },
-    xp_cd: {
-        name: "Cave Dweller",
-        creator: "XP GAMES",
-    },
-    ulkd_ess: {
-        name: "Essentials",
-        creator: "UNLINKED",
-    },
-    ulkd_alch: {
-        name: "Alchemy",
-        creator: "Unlinked",
+    andexdb: {
+        name: "8Crafter's Server Utilities & Debug Sticks",
+        creator: "8Crafter",
     },
     aria_pp: {
         name: "Planes Pro",
         creator: "AriaCreations",
     },
+    ascent_paint: {
+        name: "Paint",
+        creator: "ASCENT",
+    },
+    bf_rb: {
+        name: "Biomes",
+        creator: "Block Factory",
+    },
     darkosto_elemental_crops: {
         name: "Elemental Crops",
         creator: "Darkosto",
+    },
+    hf_mzs: {
+        name: "Mowzie’s Mobs",
+        creator: "Honeyfrost",
     },
     nps_mot: {
         name: "More Ores and Tools",
@@ -74,33 +65,9 @@ export const Registry = {
         name: "FARMING",
         creator: "Podcrash",
     },
-    bf_rb: {
-        name: "Biomes",
-        creator: "Block Factory",
-    },
-    andexsa: {
-        name: "8Crafter's Entity Scale, NBT, and Behavior Modifier, Bossbar, and Morph Addon",
-        creator: "8Crafter",
-    },
-    andexdb: {
-        name: "8Crafter's Server Utilities & Debug Sticks",
-        creator: "8Crafter",
-    },
-    wypnt_ef: {
-        name: "Epic Fantasy",
-        creator: "Waypoint Studios",
-    },
-    squaredreams_fhd: {
-        name: "Furniture HD",
-        creator: "Square Dreams",
-    },
-    squaredreams_realism: {
-        name: "Realism+",
-        creator: "Square Dreams",
-    },
-    squaredreams_bam: {
-        name: "Morph Into Anything",
-        creator: "Square Dreams",
+    spark_portals: {
+        name: "Spark Portals",
+        creator: "Spark Universe",
     },
     spark: {
         name: "RealismCraft",
